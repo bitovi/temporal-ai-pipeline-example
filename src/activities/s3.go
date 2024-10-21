407a8ce8c7b3421ce2be742c895a60078a1b8eec
NauanyCMiro <nauany.costa@miro.com>
2024-10-21 17:04:50 -0300

    refactor: use localstack

--- src/activities/s3.go
+++ src/activities/s3.go
@@ -4,22 +4,38 @@ import (
 	"bytes"
 	"context"
 	"log"
+	"os"
 
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+
 	"go.temporal.io/sdk/activity"
 )
 
+var AWS_URL string = os.Getenv("AWS_URL")
+var AWS_ACCESS_KEY_ID string = os.Getenv("AWS_ACCESS_KEY_ID")
+var AWS_SECRET_ACCESS_KEY string = os.Getenv("AWS_SECRET_ACCESS_KEY")
+
 // TODO: Update all error handling cases
-func getClient(ctx context.Context) (*s3.Client, error) {
+func getClient(ctx context.Context) (*s3.S3, error) {
 	logger := activity.GetLogger(ctx)
-	cfg, err := config.LoadDefaultConfig(context.TODO())
+	sess, err := session.NewSession(&aws.Config{
+		Region:           aws.String("us-east-1"),
+		Credentials:      credentials.NewStaticCredentials(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, ""),
+		S3ForcePathStyle: aws.Bool(true),
+		Endpoint:         aws.String(AWS_URL),
+	})
 	if err != nil {
 		logger.Error("Error from getClient:", err)
 		return nil, err
 	}
-	return s3.NewFromConfig(cfg), nil
+
+	// Create S3 service client
+	client := s3.New(sess)
+
+	return client, nil
 }
 
 type CreateS3BucketInput struct {
@@ -33,7 +49,7 @@ func CreateS3Bucket(ctx context.Context, input CreateS3BucketInput) error {
 		logger.Error("Error from getClient:", err)
 		return err
 	}
-	_, err = s3Client.CreateBucket(ctx, &s3.CreateBucketInput{Bucket: &input.Bucket})
+	_, err = s3Client.CreateBucket(&s3.CreateBucketInput{Bucket: &input.Bucket})
 	if err != nil {
 		log.Fatal("Error from CreateBucket:", err)
 		return err
@@ -54,7 +70,7 @@ func putS3Object(ctx context.Context, input PutS3ObjectInput) error {
 		logger.Error("Error from getClient:", err)
 		return err
 	}
-	_, err = s3Client.PutObject(ctx, &s3.PutObjectInput{
+	_, err = s3Client.PutObject(&s3.PutObjectInput{
 		Bucket: aws.String(input.Bucket),
 		Key:    aws.String(input.Key),
 		Body:   bytes.NewReader(input.Body),
