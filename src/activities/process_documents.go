363e5283b8a65f7f2cd747f7367a93402cc47e2b
NauanyCMiro <nauany.costa@miro.com>
2024-10-21 18:39:58 -0300

    feat: pull s3 object from aws and unzip it locally

--- src/activities/process_documents.go
+++ src/activities/process_documents.go
@@ -153,10 +153,90 @@ func ProcessDocuments(ctx context.Context, input ProcessDocumentsInput) (Process
 		}
 	}
 
-	_, err := GetS3Object(ctx, GetS3ObjectInput{s3Bucket, zipFileName})
+	response, err := GetS3Object(ctx, GetS3ObjectInput{s3Bucket, zipFileName})
+	if err != nil {
+		return ProcessDocumentsOutput{}, err
+	}
+
+	err = os.WriteFile(zipFileName, response, 0644)
+	if err != nil {
+		return ProcessDocumentsOutput{}, err
+	}
+
+	err = Unzip(zipFileName, temporaryDirectory)
+	if err != nil {
+		return ProcessDocumentsOutput{}, err
+	}
+
+	err = os.Remove(zipFileName)
+	if err != nil {
+		return ProcessDocumentsOutput{}, err
+	}
+	_, err = os.ReadDir(temporaryDirectory)
 	if err != nil {
 		return ProcessDocumentsOutput{}, err
 	}
 
 	return ProcessDocumentsOutput{TableName: "Placeholder"}, nil
+}
+
+func Unzip(src, dest string) error {
+	r, err := zip.OpenReader(src)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := r.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	os.MkdirAll(dest, 0755)
+
+	extractAndWriteFile := func(f *zip.File) error {
+		rc, err := f.Open()
+		if err != nil {
+			return err
+		}
+		defer func() {
+			if err := rc.Close(); err != nil {
+				panic(err)
+			}
+		}()
+
+		path := filepath.Join(dest, f.Name)
+		if !strings.HasPrefix(path, filepath.Clean(dest)+string(os.PathSeparator)) {
+			return fmt.Errorf("illegal file path: %s", path)
+		}
+
+		if f.FileInfo().IsDir() {
+			os.MkdirAll(path, f.Mode())
+		} else {
+			os.MkdirAll(filepath.Dir(path), f.Mode())
+			f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+			if err != nil {
+				return err
+			}
+			defer func() {
+				if err := f.Close(); err != nil {
+					panic(err)
+				}
+			}()
+
+			_, err = io.Copy(f, rc)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+
+	for _, f := range r.File {
+		err := extractAndWriteFile(f)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
