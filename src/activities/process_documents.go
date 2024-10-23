d671eb07217b9074a5eef5e68f46430f251fa5c4
NauanyCMiro <nauany.costa@miro.com>
2024-10-23 03:40:25 -0300

    feat: ensure embeddings get saved in database

--- src/activities/process_documents.go
+++ src/activities/process_documents.go
@@ -2,9 +2,12 @@ package activities
 
 import (
 	"archive/zip"
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"io"
+	"net/http"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -12,6 +15,7 @@ import (
 	"strings"
 
 	"github.com/jackc/pgx/v5"
+	"github.com/pgvector/pgvector-go"
 	pgxvector "github.com/pgvector/pgvector-go/pgx"
 	"go.temporal.io/sdk/activity"
 )
@@ -184,19 +188,22 @@ func ProcessDocuments(ctx context.Context, input ProcessDocumentsInput) (Process
 
 	for _, file := range fileList {
 		if !file.IsDir() && strings.Contains(file.Name(), ".") {
-			filePath := filepath.Join(temporaryDirectory, file.Name())
-			pageContent, err := os.ReadFile(filePath)
 
-			if err != nil {
-				return ProcessDocumentsOutput{}, fmt.Errorf("error reading file: %v", err)
-			}
+			//TODO: Remove this condition
+			if file.Name() == "uuid.md" {
+				filePath := filepath.Join(temporaryDirectory, file.Name())
+				pageContent, err := os.ReadFile(filePath)
 
-			if len(pageContent) > 0 {
-				err := saveData(ctx, vectorStoreConn, string(pageContent))
 				if err != nil {
-					return ProcessDocumentsOutput{}, fmt.Errorf("error adding document to vector store: %v", err)
+					return ProcessDocumentsOutput{}, fmt.Errorf("error reading file: %v", err)
 				}
 
+				if len(pageContent) > 0 {
+					err := saveData(ctx, vectorStoreConn, string(pageContent))
+					if err != nil {
+						return ProcessDocumentsOutput{}, fmt.Errorf("error adding document to vector store: %v", err)
+					}
+				}
 			}
 		}
 	}
@@ -307,10 +314,67 @@ func createTable(ctx context.Context, conn *pgx.Conn) {
 }
 
 func saveData(ctx context.Context, conn *pgx.Conn, input string) error {
-	_, err := conn.Exec(ctx, "INSERT INTO documents (content) VALUES ($1)", input)
+	embeddings, err := FetchEmbeddings(input)
 	if err != nil {
 		return err
 	}
+	_, err = conn.Exec(ctx, "INSERT INTO documents (content, embedding) VALUES ($1, $2)", input, pgvector.NewVector(embeddings))
+
+	if err != nil {
+		panic(err)
+	}
 
 	return nil
+}
+
+type apiRequest struct {
+	Input []string `json:"input"`
+	Model string   `json:"model"`
+}
+type embeddingResponse struct {
+	Data []struct {
+		Embedding []float32 `json:"embedding"`
+	} `json:"data"`
+}
+
+func FetchEmbeddings(input string) ([]float32, error) {
+	url := "https://api.openai.com/v1/embeddings"
+
+	data := &apiRequest{
+		Input: []string{input},
+		Model: "text-embedding-ada-002",
+	}
+
+	b, err := json.Marshal(data)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(b))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", OPENAI_API_KEY))
+	req.Header.Add("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		body, _ := io.ReadAll(resp.Body)
+		return nil, fmt.Errorf("bad status code: %d - %s", resp.StatusCode, body)
+	}
+
+	var result embeddingResponse
+	err = json.NewDecoder(resp.Body).Decode(&result)
+	if err != nil {
+		return nil, err
+	}
+
+	return result.Data[0].Embedding, nil
 }
