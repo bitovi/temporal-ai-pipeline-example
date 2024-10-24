906dce4389bdfb0b9ff7a46dceec2fff0cf0f4e3
NauanyCMiro <nauany.costa@miro.com>
2024-10-24 18:49:57 -0300

    feat: ensure fail cases run on temporal cloud

---
+++ src/invoke-prompt-fail-client.ts
@@ -0,0 +1,35 @@
+import { Connection, Client } from '@temporalio/client';
+import { invokePromptWorkflow } from './workflows';
+import { nanoid } from 'nanoid';
+import { getTemporalClientOptions } from './utils';
+
+async function run() {
+  const connection = await Connection.connect(getTemporalClientOptions());  
+  const client = new Client({ 
+    connection,
+    namespace: process.env.NAMESPACE,
+  });
+
+  const [ latestDocumentProcessingId, query, conversationId ] = process.argv.slice(2)
+
+  const id = `invoke-prompt-workflow-fail-${nanoid()}`.toLowerCase().replaceAll('_', '')
+  const handle = await client.workflow.start(invokePromptWorkflow, {
+    taskQueue: 'invoke-prompt-queue',
+    args: [{
+      query,
+      latestDocumentProcessingId,
+      conversationId,
+      failRate: 0.6
+    }],
+    workflowId: id
+  });
+
+  console.log(`Workflow ${handle.workflowId} running`);
+
+  console.log(await handle.result());
+}
+
+run().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
---
+++ src/process-documents-fail-client.ts
@@ -0,0 +1,37 @@
+import { Connection, Client } from '@temporalio/client';
+import { documentsProcessingWorkflow } from './workflows';
+import { nanoid } from 'nanoid';
+import { getTemporalClientOptions } from './utils';
+
+async function run() {
+  const connection = await Connection.connect(getTemporalClientOptions());  
+  const client = new Client({ 
+    connection,
+    namespace: process.env.NAMESPACE,
+  });
+
+  const id = `process-documents-workflow-fail-${nanoid()}`.toLowerCase().replaceAll('_', '')
+  const handle = await client.workflow.start(documentsProcessingWorkflow, {
+    taskQueue: 'documents-processing-queue',
+    args: [{
+      id,
+      repository: {
+        url: 'https://github.com/bitovi/hatchify.git',
+        branch: 'main',
+        path: 'docs',
+        fileExtensions: ['md']
+      },
+      failRate: 0.6
+    }],
+    workflowId: id
+  });
+
+  console.log(`Workflow ${handle.workflowId} running`);
+
+  console.log(await handle.result());
+}
+
+run().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
