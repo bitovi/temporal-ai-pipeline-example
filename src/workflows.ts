692174fb732280f7615404bde5599943a8e69eb0
NauanyCMiro <nauany.costa@miro.com>
2024-10-25 15:19:47 -0300

    refactor: remove retry policy configs temporarily

--- src/workflows.ts
+++ src/workflows.ts
@@ -3,29 +3,14 @@ import * as activities from './activities'
 
 const { createS3Bucket, deleteS3Object, deleteS3Bucket, generatePrompt, invokePrompt, loadTestCases } = proxyActivities<typeof activities>({
   startToCloseTimeout: '1 minute',
-  retry: 
-  {
-    backoffCoefficient: 1,
-    initialInterval: '3 seconds'
-  }
 })
 
 const { collectDocuments, validateQueryResult, summarizeValidationResults } = proxyActivities<typeof activities>({
-  startToCloseTimeout: '5 minute',
-  retry: 
-  {
-    backoffCoefficient: 1,
-    initialInterval: '3 seconds'
-  }
+  startToCloseTimeout: '5 minute', 
 })
 
 const { processDocuments } = proxyActivities<typeof activities>({
-  startToCloseTimeout: '50 minute',
-  retry: 
-  {
-    backoffCoefficient: 1,
-    initialInterval: '3 seconds'
-  }
+  startToCloseTimeout: '50 minute', 
 })
 
 type Repository = {
